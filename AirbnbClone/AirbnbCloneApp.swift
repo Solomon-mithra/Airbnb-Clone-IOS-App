daa2f911ed1a59b90866c290ace2df7a9531a4ca
codythefennec <101runt52@gmail.com>
2025-03-04 14:03:52 -0800

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Yusif Imanov on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
